260dfb751b2169dedabf3ea10483b96e9933c63d
LuanvSilva <luanlvs2010@gmail.com>
2024-04-30 20:40:10 -0300

    feat: add new desafio: Selecao de Candidatos

---
+++ src/main/java/com/santader_bootcamp/santader_bootcamp/desafios/SelecaoCadidatos.java
@@ -0,0 +1,66 @@
+package com.santader_bootcamp.santader_bootcamp.desafios;
+
+import java.util.ArrayList;
+
+import org.hibernate.mapping.List;
+
+public class SelecaoCadidatos {
+    
+    public static void main(String[] args) {
+        selecaoCadidatos();
+    }
+
+    static void selecaoCadidatos(){
+        String[] candidatos = {"João", "Maria", "José", "Pedro", "Paulo", "Ana", "Carlos", "Mariana", "Felipe", "Cristina"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        int index = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5) {
+
+            ArrayList<String> candidato = new ArrayList<String>();
+            candidato.add(candidatos[candidatosAtual]);
+            double salarioPretendito = valorPretendito();
+
+            System.out.println("O candidato " + candidatos[candidatosAtual] + " pretende ganhar " + salarioPretendito);
+
+            if (salarioBase >= salarioPretendito) {
+                System.out.println("O candidato " + candidato.get(index) + " foi selecionado");
+                candidatosSelecionados++;
+            } else {
+                System.out.println("O candidato " + candidato.get(index) + " não foi selecionado");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendito(){
+        double salarioPretendito = Math.random() * 10000;
+        return salarioPretendito;
+    }
+
+    static void analizarCandidato(double salarioPretendito){
+
+        double salariobase = 2000.0;
+
+        if (salariobase > salarioPretendito) {
+            System.out.println("Ligar para o candidato");
+        }
+        else if(salariobase == salarioPretendito){
+            System.out.println("Ligar para o candidato e negociar");
+        }
+        else{
+            System.out.println("Não ligar para o candidato");
+        }
+    }
+
+
+
+
+
+   
+    
+}
